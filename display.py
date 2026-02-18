bf02d1074adc97c80f208e780b140f41d739f12b
BaileyRobison <robi5895@gmail.com>
2026-02-18 16:37:55 -0500

    colors in yaml file

--- display.py
+++ display.py
@@ -3,18 +3,33 @@ Basic display in matplotlib
 """
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
+import yaml
+import os
+
+def read_yaml(yaml_file):
+    # path to yaml file
+    current_dir = os.path.dirname(__file__)
+    path = current_dir+'/config/'+yaml_file+'.yaml'
+    
+    # open yaml file
+    with open(path, encoding= 'utf-8') as f:
+        yaml_content = yaml.safe_load(f)
+        
+    return yaml_content
 
 def run_stock(market, ticks = 100, duration = 0.1):
+    colors = read_yaml('style')
+    
     prices = [market.stocks[0].price] # initial price
     times = [datetime(2026,1,1,12,0)]
 
     plt.ion() # enter interactive mode
-    fig, ax = plt.subplots(facecolor='#303030')
-    ax.set_facecolor('#303030')
+    fig, ax = plt.subplots(facecolor=colors['bg'])
+    ax.set_facecolor(colors['bg'])
     
     for side in ['bottom', 'top', 'left', 'right']:
-        ax.spines[side].set_color('white')
-    ax.tick_params(axis='both', color='white', labelcolor='white')
+        ax.spines[side].set_color(colors['axes'])
+    ax.tick_params(axis='both', color=colors['axes'], labelcolor=colors['axes'])
     
     for tick in range(ticks): # random updates to price
         market.update_stock()
@@ -30,9 +45,9 @@ def run_stock(market, ticks = 100, duration = 0.1):
         curr = prices[tick]
 
         if curr > prev:
-            color = '#90EE90'
+            color = colors['green_bar']
         else:
-            color = '#FF474C'
+            color = colors['red_bar']
             
         # plot new bar
         ax.bar(tick, curr - prev, bottom=prev, color=color, width=0.8)
